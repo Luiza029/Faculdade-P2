c756778b60a5015313612441142a4f4105cd4622
Luiza Costa <cwluiza029@gmail.com>
2026-03-31 21:30:40 -0300

    Sub string

--- C/PIF/exercicios/String/EDEN.c
+++ C/PIF/exercicios/String/EDEN.c
@@ -1,25 +1,63 @@
 #include <stdio.h>
 
-void ajustar(int *a, int *b){
-    if(*a > *b){
-        *a = *a - 1;
-        *b = *b + 1;
+char* maiorSubstringVogais(char *S, int *maior) {
+    int atual = 0;   
+    *maior = 0;  
 
-    } if(*a < *b){
-        *a = *a + 1;
-        *b = *b - 1;
-    } if(*a == *b){
-        *a = *a;
-        *b = *b;
+    char *p = S;
+    char *indice_atual = S;
+    char *indice_maior = S;
+
+    while(*p != '\0'){    
+            if(*p == 'a' || *p == 'e' || *p == 'i' || *p == 'o' ||*p == 'u'){
+                if(atual == 0){
+                    indice_atual = p;
+                }
+
+                atual++;
+
+                if(atual > *maior){
+                    *maior = atual;
+                    indice_maior = indice_atual;
+                }
+            } else{
+                atual = 0;
+            }
+
+        p++;
     }
+    return indice_maior; 
 }
 
+char* copiar(char *saida, char *S, int n) {
+    char *inicio_saida = saida;
+
+    for (int i = 0; i < n; i++) {
+        *saida = *S;
+        saida++;
+        S++;
+    }
+    *saida = '\0';
+
+    return inicio_saida;
+}
+
+
 int main(void) {
-    int A, B;
-    if (scanf("%d %d", &A, &B) != 2) return 0;
 
-    ajustar(&A, &B);
+    char str[100];
+    char saida[100];
 
-    printf("%d %d\n", A, B);
+    fgets(str,99,stdin);
+
+    int tamanho; 
+    char *inicio = maiorSubstringVogais(str, &tamanho);
+
+    printf("%d",tamanho);
+
+    copiar(saida,inicio, tamanho);
+    printf("\n%s\n",saida);
+
     return 0;
+
 }
