7538d1e0a5b0180de9f83aa337386778db754bec
Luiza Costa <cwluiza029@gmail.com>
2026-04-07 16:36:38 -0300

    arrumando erros

--- C/PIF/exercicios/Lista 01/14blocoContinuo.c
+++ C/PIF/exercicios/Lista 01/14blocoContinuo.c
@@ -3,31 +3,27 @@
 #include <string.h>
 
 int main() {
-    int linha, coluna, **matriz, i, j, soma = 0;
+    int linha, coluna, *matriz, i, j, soma = 0;
 
     printf("Digite o tamanho de linha e da coluna: ");
     scanf("%d %d",&linha, &coluna);
 
-    matriz = (int**) malloc((linha * coluna) * sizeof(int*));
+    matriz = (int*) malloc((linha * coluna) * sizeof(int));
 
     for(i=0; i<linha; i++){
         for(j=0; j<coluna; j++){
             printf("Preencha o elemento [%d, %d]: ", i, j);
-            scanf("%d", &matriz[i][j]);
+            scanf("%d", &matriz[i * coluna + j]);
         }
     }
     
     for(i=0; i<linha; i++){
         for(j=0; j<coluna; j++){
-            soma += matriz[i][j];
+            soma += matriz[i * coluna + j];
         }
     }
 
     printf("Soma: %d", soma);
-
-    for(i=0; i<linha; i++){
-        free(matriz[i]);
-    }
 
     free(matriz);
     return 0;
